2f7de808db0e212a6126cd3bb23ba6a661592687
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-10-15 09:13:35 +0900

    Add a description in Config.cs

--- LobbyMusicPlugin/Config.cs
+++ LobbyMusicPlugin/Config.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Exiled.API.Interfaces;
 
 namespace LobbyMusicPlugin
@@ -6,8 +7,11 @@ namespace LobbyMusicPlugin
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
+        [Description("Enable loop the one specific song?")]
         public bool LoopSingleSong { get; set; } = true;
+        [Description("Choose a loop specific song")]
         public string SingleSongName { get; set; } = "77.ogg";
+        [Description("If the song is not loop, the song will be play in this list")]
         public string[] QueueSongs { get; set; } = { "Example1.ogg", "Example2.ogg" };
 
     }
