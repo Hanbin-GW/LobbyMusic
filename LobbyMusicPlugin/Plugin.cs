481096bb712d6f73cdb5d0003970bac078cd4a62
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-10-15 09:13:48 +0900

    Version Up

--- LobbyMusicPlugin/Plugin.cs
+++ LobbyMusicPlugin/Plugin.cs
@@ -11,7 +11,7 @@ namespace LobbyMusicPlugin
     {
         public override string Name { get; } = "LobbyMusicPlugin";
         public override string Author { get; } = "Hanbin-GW";
-        public override Version Version { get; } = new Version(0, 1, 0);
+        public override Version Version { get; } = new Version(0, 1, 1);
         public static Plugin Instance { get; private set; }
         private readonly string _audioDirectory;
         private bool _isMusicPlaying = false;
