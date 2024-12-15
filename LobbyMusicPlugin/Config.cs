973ab3ca8897a5822fa8ee2eb6904c1065fe3deb
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-12-15 13:56:33 +0900

    Working on rework... (It's so hard..)

--- LobbyMusicPlugin/Config.cs
+++ LobbyMusicPlugin/Config.cs
@@ -10,7 +10,7 @@ namespace LobbyMusicPlugin
         [Description("Enable loop the one specific song?")]
         public bool LoopSingleSong { get; set; } = true;
         [Description("Choose a loop specific song")]
-        public string SingleSongName { get; set; } = "77.ogg";
+        public string SingleSongName { get; set; } = "77.opus";
         [Description("If the song is not loop, the song will be play in this list")]
         public string[] QueueSongs { get; set; } = { "Example1.ogg", "Example2.ogg" };
         [Description("You can set a standard volume when music is playing.")]
