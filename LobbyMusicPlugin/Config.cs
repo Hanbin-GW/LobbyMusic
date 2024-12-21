34b136b7337ba4b0f362b5aae5d20e1542ac18c5
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-12-21 19:46:16 +0900

    Music System Rework "IS NOT PUBLIC"

--- LobbyMusicPlugin/Config.cs
+++ LobbyMusicPlugin/Config.cs
@@ -7,16 +7,7 @@ namespace LobbyMusicPlugin
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
-        [Description("Enable loop the one specific song?")]
-        public bool LoopSingleSong { get; set; } = true;
         [Description("Choose a loop specific song")]
-        public string SingleSongName { get; set; } = "77.opus";
-        [Description("If the song is not loop, the song will be play in this list")]
-        public string[] QueueSongs { get; set; } = { "Example1.ogg", "Example2.ogg" };
-        [Description("You can set a standard volume when music is playing.")]
-        public float StandardVolume { get; set; } = 70f;
-
-        [Description("Play music without player?")]
-        public bool IsPlayMusicNoPerson { get; set; } = false;
+        public string Path { get; set; } = "77.opus";
     }
 }
--- LobbyMusicPlugin/Plugin.cs
+++ LobbyMusicPlugin/Plugin.cs
@@ -1,37 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using AdminToys;
+using System.IO;
+using Exiled.API.Enums;
 using Exiled.API.Features;
-using VoiceChat.Codec;
-using VoiceChat.Codec.Enums;
+using Exiled.API.Features.Toys;
+using PlayerRoles;
+using UnityEngine;
+using VoiceChat.Networking;
+using VoiceChat.Playbacks;
 
 namespace LobbyMusicPlugin
 {
-    public class LobbyMusic : Plugin<Config>
+    public class Plugin : Plugin<Config>
     {
-        public override string Name => "LobbyMusic";
-        public override string Author => "YourName";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override string Name { get; } = "Lobby Music - 14.0";
+        public override string Author { get; } = "Hanbin - GW";
+        public override Version Version { get; } = new Version(0, 2, 3);
+        public bool IsMusicPlaying = false;
+        private Speaker _speaker;
+        private readonly string _audioDirectory;
+        public static Plugin Instance { get; private set; }
         
-        private void OnWaitingForPlayers()
+        public Plugin()
         {
-            SpeakerManager.Instance.EnsureMusicDirectoryExists();
-            //PlayLobbyMusic();
-            if (Server.PlayerCount == 0)
-            {
-                SpeakerManager.Instance.PlayMusicToPlayer("77.ogg");
-            }
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            _audioDirectory = Path.Combine(appDataPath, "EXILED", "Plugins", "audio");
         }
+
+
         public override void OnEnabled()
         {
-            Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingForPlayers;
-            base.OnEnabled();
+            if (Server.IpAddress == "121.166.155.25" && Server.Port == 7779)
+            {
+                Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+                Exiled.Events.Handlers.Server.WaitingForPlayers += OnWaitingPlayers;
+                base.OnEnabled();
+            }
+            else
+            {
+                Log.Error("THIS PLUGIN IS NOT ALLOW TO USE");
+            }
         }
 
         public override void OnDisabled()
         {
-            Exiled.Events.Handlers.Server.WaitingForPlayers -= OnWaitingForPlayers;
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= OnWaitingPlayers;
             base.OnDisabled();
+        }
+
+        private void OnWaitingPlayers()
+        {
+            EnsureMusicDirectoryExists();
+            Vector3 position = new Vector3(0, 5, 0);
+            Vector3 rotation = new Vector3(0, 90, 0);
+            Vector3 scale = new Vector3(1, 1, 1);
+            string songPath = Path.Combine(_audioDirectory, Config.Path); 
+
+            _speaker = Speaker.Create(position, rotation, scale, spawn: true);
+            _speaker.IsSpatial = false;
+            byte[] opusData = File.ReadAllBytes(songPath);
+            AudioMessage msg = new AudioMessage
+            {
+                Data = opusData,
+                DataLength = opusData.Length,
+            };
+            _speaker.Base.Playback.DecodeSamples(msg);
+            Log.Info($"playing... {msg}");
+        }
+
+        private void OnRoundStarted()
+        {
+            if (_speaker != null)
+            {
+                _speaker.Destroy();
+            }
+            RoleTypeId npcRole = RoleTypeId.Scp049;
+            Npc npc = Npc.Spawn("TestNPC", npcRole, new Vector3(0, 0, 0));
+            Log.Info($"NPC {npc.Nickname}가 생성되었습니다.");
+            npc.Teleport(RoomType.LczClassDSpawn);
+            npc.LateDestroy(110f);
+        }
+        
+        private void EnsureMusicDirectoryExists()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EXILED", "Plugins", "audio");
+
+            // 폴더가 없으면 생성
+            if (!Directory.Exists(path))
+            {
+                Log.Warn($"음악 폴더가 존재하지 않습니다. 새로 생성합니다: {path}");
+                Directory.CreateDirectory(path);  // 폴더 생성
+            }
+            else
+            {
+                Log.Info("음악 폴더가 이미 존재합니다.");
+            }
         }
     }
 }
